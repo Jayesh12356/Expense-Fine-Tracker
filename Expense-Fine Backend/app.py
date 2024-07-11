7301e510b795c2f98f712e0355799a3da392ac0a
Jayesh Koli <Jayesh.Koli@ril.com>
2024-07-11 15:54:09 +0530

    New Upgrades

--- Expense-Fine Backend/app.py
+++ Expense-Fine Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask,request, jsonify
 import json
 from flask_cors import CORS
 import requests
@@ -48,8 +48,10 @@ def status_report():
         remote_api_url = 'http://canteen.benzyinfotech.com/api/v3/customer/report'
 
         #TOKEN WAS EXPIRED
+        month = request.args.get('month', default=11, type=int)
+        print(month)
         auth_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZWRhNWExODU0OTFhYWE0MmY5YzMyZjRhMTU5MDM1ODk4ZjZiMzMxNWUzZjJjNGRiZDA1N2IyNGE3NTAzMDc3NDBlMjFlYjZmNGE4Mjk0MGUiLCJpYXQiOjE3MDQ4MDA4OTAuODc5OTI1OTY2MjYyODE3MzgyODEyNSwibmJmIjoxNzA0ODAwODkwLjg3OTkyOTA2NTcwNDM0NTcwMzEyNSwiZXhwIjoxNzM2NDIzMjkwLjgzNDkxMjA2MTY5MTI4NDE3OTY4NzUsInN1YiI6IjI2NSIsInNjb3BlcyI6W119.CwDEjlHoRtOXdFcaO6KGGxV202AOA7MMtJVPtKzgLqzTFzUUnDLGBd7PNAtHO2--3YOathM9HOG8hYjY8wjktXZIoCGUR9GWIaEVUxLwFq927CrSf05NuqTBTrJcDeBOjXDvKcSBiJ2A994FC2IunPcdkaZ4jpoaWBIaWueYUbHviYSQuLec3tFcAMg4njrImAlaN9kQKkHetpdrdbUEX1Wzq4X1QwuOx7W3W2nbbxaoNgFX1gaabxi00ZO7h5MokGvtqy_gCkS9TYoM74VfxmTyAAczjttLcPqDNiAL_ZJdutDMezw32CZj8G8l8PUL46F_BuaxatZDBUZxeClZh4_0Wvo9GX4zqF2XvHdzZHnwdB414vNCl8itaGW9w7QWbdchPOglhnek32ZmkH0MIqeOBhnAyHo5_WbP0uLd_3qmz3w04nvTbTGV25-QebaxPAsVD0-7Za1sVpqB_FD6yEeliaEzdxl_8gA5IH59uowpfPYgUIjom8NVEASuYsAwb0q3f0jhNRfwg2zmXNenoDunh_dN9l2NRjI2gdZueSMwu6IJLQK46jpn01uG2iQ1xxpFJAGe_bzSceLsho3dbtabym3tMqi0Ac02xUP9Mn50LdkFJGNVU9jiuHQfyjQirDtGUfya3aIvpJlCGx9Cx99s_4P89uDnOiXy3A1Q"
-        params = {"month": 11}
+        params = {"month": month}
         
         headers = {
             'Authorization': f'Bearer {auth_token}',
--- Expense-Fine UI/src/index.js
+++ Expense-Fine UI/src/index.js
@@ -10,6 +10,7 @@ function App() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [month, setMonth] = useState("");
 
   function handleShowUserDetails() {
     setShowUserDetails(!showUserDetails);
@@ -29,24 +30,167 @@ function App() {
     setShowUserDetails(false);
   }
 
-  useEffect(function () {
-    async function fetchFine() {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-        const res = await fetch("http://127.0.0.1:5000/status_report");
-        const data = await res.json();
-        setData(data);
-      } catch (err) {
-        setIsError(true);
-        console.error(err.message);
-      } finally {
-        setIsLoading(false);
+  useEffect(
+    function () {
+      if (!month) return;
+      async function fetchFine() {
+        try {
+          setIsLoading(true);
+          setIsError(false);
+          const res = await fetch(
+            `http://127.0.0.1:5000/status_report?month=${month}`
+          );
+          const data = await res.json();
+          setData(data);
+        } catch (err) {
+          setIsError(true);
+          console.error(err.message);
+        } finally {
+          setIsLoading(false);
+        }
       }
-    }
 
-    fetchFine();
-  }, []);
+      fetchFine();
+    },
+    [month]
+  );
+
+  // return (
+  //   <div className="app-container">
+  //     {isError && (
+  //       <div className="loader-container">
+  //         <p>No Data Found</p>
+  //       </div>
+  //     )}
+  //     {isLoading && !isError && (
+  //       <div className="loader-container">
+  //         <p>Loading . . .</p>
+  //       </div>
+  //     )}
+
+  //     {!isLoading && !isError && (
+  //       <div className="component-container">
+  //         <span>Select Month</span>
+  //         <select
+  //           value={month}
+  //           onChange={(e) => {
+  //             setMonth(Number(e.target.value));
+  //           }}
+  //         >
+  //           {Array.from({ length: 12 }, (_, i) => i + 1).map((num) => (
+  //             <option value={num} key={num}>
+  //               {num}
+  //             </option>
+  //           ))}
+  //         </select>
+  //         <div className="button-container">
+  //           <button className="button" onClick={handleShowUserDetails}>
+  //             Show User Details
+  //           </button>
+  //           <button className="button" onClick={handleShowExpenses}>
+  //             Show Expenses
+  //           </button>
+  //           <button className="button" onClick={handleShowMonthlyFines}>
+  //             Show Monthly Fines
+  //           </button>
+  //         </div>
+  //       </div>
+  //     )}
+  //     <div
+  //       className={`data-container ${
+  //         showUserDetails || showExpenses || showMonthlyFines ? "wide" : ""
+  //       }`}
+  //     >
+  //       {showUserDetails && data && (
+  //         <div className="user-details show">
+  //           <h2>User Details</h2>
+  //           <table className="user-details-table">
+  //             <tbody>
+  //               <tr>
+  //                 <th>Created At</th>
+  //                 <td>{data.user_data.created_at}</td>
+  //               </tr>
+  //               <tr>
+  //                 <th>Email</th>
+  //                 <td>{data.user_data.email}</td>
+  //               </tr>
+  //               <tr>
+  //                 <th>Employee ID</th>
+  //                 <td>{data.user_data.emp_id}</td>
+  //               </tr>
+  //               <tr>
+  //                 <th>Name</th>
+  //                 <td>
+  //                   {data.user_data.f_name} {data.user_data.l_name}
+  //                 </td>
+  //               </tr>
+  //               <tr>
+  //                 <th>Phone</th>
+  //                 <td>{data.user_data.phone}</td>
+  //               </tr>
+  //             </tbody>
+  //           </table>
+  //         </div>
+  //       )}
+
+  //       {showExpenses && data && (
+  //         <>
+  //           <h2>Expenses:</h2>
+  //           <div className="expenses-details show">
+  //             <table>
+  //               <thead>
+  //                 <tr>
+  //                   <th>Date</th>
+  //                   <th>Breakfast</th>
+  //                   <th>Lunch</th>
+  //                   <th>Dinner</th>
+  //                   <th>Fine</th>
+  //                 </tr>
+  //               </thead>
+  //               <tbody>
+  //                 {data.daily_status.map((status, index) => (
+  //                   <tr key={index}>
+  //                     <td>{status.date}</td>
+  //                     <td>{status.breakfast}</td>
+  //                     <td>{status.lunch}</td>
+  //                     <td>{status.dinner}</td>
+  //                     <td>{status.fine}</td>
+  //                   </tr>
+  //                 ))}
+  //               </tbody>
+  //             </table>
+  //           </div>
+  //         </>
+  //       )}
+
+  //       {showMonthlyFines && data && (
+  //         <>
+  //           <h2>Monthly Fines:</h2>
+  //           <div className="monthly-fines-details show">
+  //             <table>
+  //               <thead>
+  //                 <tr>
+  //                   <th>Year</th>
+  //                   <th>Month</th>
+  //                   <th>Fine</th>
+  //                 </tr>
+  //               </thead>
+  //               <tbody>
+  //                 {data.monthly_fines.map((fine, index) => (
+  //                   <tr key={index}>
+  //                     <td>{fine.year}</td>
+  //                     <td>{fine.month}</td>
+  //                     <td>{fine.fine}</td>
+  //                   </tr>
+  //                 ))}
+  //               </tbody>
+  //             </table>
+  //           </div>
+  //         </>
+  //       )}
+  //     </div>
+  //   </div>
+  // );
 
   return (
     <div className="app-container">
@@ -63,6 +207,21 @@ function App() {
 
       {!isLoading && !isError && (
         <div className="component-container">
+          <div className="select-month-container">
+            <span>Select Month</span>
+            <select
+              value={month}
+              onChange={(e) => {
+                setMonth(Number(e.target.value));
+              }}
+            >
+              {Array.from({ length: 12 }, (_, i) => i + 1).map((num) => (
+                <option value={num} key={num}>
+                  {num}
+                </option>
+              ))}
+            </select>
+          </div>
           <div className="button-container">
             <button className="button" onClick={handleShowUserDetails}>
               Show User Details
